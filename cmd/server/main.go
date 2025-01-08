5c590e4cf9661da06e7450e058f49a30742566ca
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2025-01-08 11:27:10 -0300

    feat: connect to RabbitMQ

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const (
+	amqpURI = "amqp://guest:guest@localhost:5672/"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	log.Println("Starting Peril server...")
+
+	conn, err := amqp.Dial(amqpURI)
+	if err != nil {
+		log.Fatalln("Failed to connect to RabbitMQ:", err)
+		return
+	}
+	defer conn.Close()
+	log.Println("Connected to RabbitMQ!")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	sig := <-signalChan
+	log.Printf("Received signal: %v\n", sig)
+	log.Println("Closing connection...")
+	conn.Close()
+	log.Println("Connection closed!")
+	os.Exit(0)
 }
