df49a4f503fe99f99348db031bc7679b957db2d0
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2025-01-08 12:10:39 -0300

    feat: client subscribe to pause key

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const (
+	amqpURI = "amqp://guest:guest@localhost:5672/"
+)
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	log.Println("Starting Peril client...")
+
+	conn, err := amqp.Dial(amqpURI)
+	if err != nil {
+		log.Fatalln("Failed to connect to RabbitMQ:", err)
+		return
+	}
+	defer conn.Close()
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalln("Failed to get username:", err)
+		return
+	}
+
+	ch, queue, err := pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, fmt.Sprintf("%s.%s", routing.PauseKey, username), routing.PauseKey, pubsub.Transient)
+	if err != nil {
+		log.Fatalln("Failed to declare and bind queue to exchange:", err)
+		return
+	}
+	messagesChan, err := ch.Consume(queue.Name, "", true, false, false, false, nil)
+	if err != nil {
+		log.Fatalln("Failed to consume messages from queue:", err)
+		return
+	}
+
+	message := <-messagesChan
+	log.Printf("Received message: %s\n", message.Body)
 }
